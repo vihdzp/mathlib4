19aa712ef324725391ca162fe844afc042520473
Violeta Hernández Palacios <vi.hdz.p@gmail.com>
2026-01-29 05:54:28 -0600

    Update Mathlib/Data/Real/Archimedean.lean
    Co-authored-by: Anne Baanen <Vierkantor@users.noreply.github.com>

--- Mathlib/Data/Real/Archimedean.lean
+++ Mathlib/Data/Real/Archimedean.lean
@@ -50,7 +50,7 @@ theorem of_near (f : ℕ → ℚ) (x : ℝ) (h : ∀ ε > 0, ∃ i, ∀ j ≥ i,
         (eq_of_le_of_forall_lt_imp_le_of_dense (abs_nonneg _)) fun _ε ε0 =>
           mk_near_of_forall_near <| (h _ ε0).imp fun _i h j ij => le_of_lt (h j ij)⟩
 
-@[deprecated "use `⌊x⌋` directly" (since := "2026-01-29")]
+@[deprecated "use `⌊x⌋`, `Int.floor_le` and `Int.le_floor` directly" (since := "2026-01-29")]
 theorem exists_floor (x : ℝ) : ∃ ub : ℤ, (ub : ℝ) ≤ x ∧ ∀ z : ℤ, (z : ℝ) ≤ x → z ≤ ub :=
   ⟨⌊x⌋, Int.floor_le x, fun _ ↦ Int.le_floor.mpr⟩
 
