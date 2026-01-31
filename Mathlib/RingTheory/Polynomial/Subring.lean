7f83f235ef813e472d40aab6ad8c61333be48c2e
Violeta Hernández Palacios <vi.hdz.p@gmail.com>
2026-01-31 16:00:36 -0600

    add doc

--- Mathlib/RingTheory/Polynomial/Subring.lean
+++ Mathlib/RingTheory/Polynomial/Subring.lean
@@ -11,6 +11,21 @@ public import Mathlib.Algebra.Ring.Subring.Defs
 
 import Mathlib.Algebra.Polynomial.Eval.Coeff
 
+/-!
+# Polynomials over subrings
+
+Given a ring `K` with a subring `R`, we construct a map from polynomials in `K[X]` with coefficients
+in `R` to `R[X]`. We provide several lemmas to deal with coefficients, degree, and evaluation of
+`Polynomial.toSubring`.
+
+## Main Definitions
+
+* `Polynomial.toSubring` : given a polynomial `P` in `K[X]` whose coefficients all belong to a
+  subring `R` of the ring `K`, `P.toSubring R` is the corresponding polynomial in `R[X]`.
+-/
+
+@[expose] public section
+
 namespace Polynomial
 
 variable {R : Type*} [Ring R] (p : R[X]) (T : Subring R)
