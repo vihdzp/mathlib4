d6ee94c4be962bfcfc3768de9d724d5efa0329c9
vihdzp <vi.hdz.p@mgmail.com>
2026-02-04 19:36:08 -0600

    pr

--- Mathlib.lean
+++ Mathlib.lean
@@ -6349,7 +6349,6 @@ public import Mathlib.RingTheory.Polynomial.Selmer
 public import Mathlib.RingTheory.Polynomial.SeparableDegree
 public import Mathlib.RingTheory.Polynomial.ShiftedLegendre
 public import Mathlib.RingTheory.Polynomial.SmallDegreeVieta
-public import Mathlib.RingTheory.Polynomial.Subring
 public import Mathlib.RingTheory.Polynomial.Tower
 public import Mathlib.RingTheory.Polynomial.UniqueFactorization
 public import Mathlib.RingTheory.Polynomial.UniversalFactorizationRing
