e2937da1dd28190d669c3056b0e963952ec06717
vihdzp <vi.hdz.p@gmail.com>
2026-02-27 01:31:41 -0600

    no public decls

--- Mathlib/Order/IsNormal.lean
+++ Mathlib/Order/IsNormal.lean
@@ -21,7 +21,7 @@ bound of `f b` with `b < a`.
 See `Order.isNormal_iff_strictMono_and_continuous` for a proof that these notions are equivalent.
 -/
 
-@[expose] public section
+public section
 
 open Order Set
 
